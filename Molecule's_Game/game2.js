b4ad66a8362580912619ee7f9a5d7def9789b0d2
SF-rj <Lefever@etown.edu>
2025-08-04 08:26:12 -0700

    updating my code for molecules mission

--- Molecule's_Game/game2.js
+++ Molecule's_Game/game2.js
@@ -3,27 +3,51 @@ let canvas = document.querySelector("canvas");
 //Gets the graphics
 const graphics = canvas.getContext("2d");
 //Declare Variables
-let x = 100;
-let y = 100;
-window.setInterval(animation,1000/fps);
-function animation(){
+let rockX= 800;
+let rockY = 479;
+let y = 420;
+
+
+
+
+
+
+function animation()
+{
 clear();
+rock();
+rockX -= 100;
 block();
-move();
+jump();
 }
-function move(){
-x+= 40;
-y+= 40;
-}
-//Ball
-function block(){
-graphics.fillStyle = "#00C15D";
-graphics.fillRect(x,y,80,80);
-    
+
+
+function rock()
+{
+    graphics.fillStyle = "gray";
+    graphics.beginPath();
+    graphics.arc(rockX,rockY,20,0,Math.PI*2);
+    graphics.fill();
+    graphics.closePath();
 
 }
+//Ball
+function block()
+{
+graphics.fillStyle = "#00C15D";
+graphics.fillRect(100,420,80,80);
+}
 //Clear
-function clear(){
+function clear()
+{
     graphics.fillStyle = "#FFEEDD";
     graphics.fillRect(0,0,canvas.width,canvas.height);
 }
+
+
+
+
+
+
+
+window.setInterval(animation,500);
