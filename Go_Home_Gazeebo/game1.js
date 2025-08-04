c1228b7ff36e8d92d7b33872222550b61d49d37d
TStarfall-Luca <suliveras@etown.edu>
2025-08-04 10:07:30 -0400

    adding layout comments

--- Go_Home_Gazeebo/game1.js
+++ Go_Home_Gazeebo/game1.js
@@ -1,1 +1,15 @@
-//Luca
+/*Luca
+
+LAYOUT FOR GO HOME GAZEEBO:
+    Farm (Part 1)
+    City
+    Mesa Canyon (Part 2)
+    Beach
+    Underwater City (Part 3)
+    Forest
+    Jungle (Part 4)
+    Snowy Plains
+    Mountain (Part 5)
+    End Scene (From Mountain Top)*/
+
+    
