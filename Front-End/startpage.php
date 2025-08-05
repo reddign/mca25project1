846075983425b498718cf6f455bef0f1776b04ea
Jayla <delgado@etown.edu>
2025-08-05 09:44:12 -0400

    Adding css to one of the pages

--- Front-End/startpage.php
+++ Front-End/startpage.php
@@ -8,23 +8,25 @@
 </head>
 
 <body>
-    <h1>Home Page</h1>
+      <h1>Home Page</h1>
+     <li class: logo> 
+        <ul>
+                <!-- this is just a filler link until we get a better web space. -->
+        <a href="http://127.0.0.1/MCA25PROJECT1/Front-End/login.php" style="margin-top:0px">
+             <img id="logo" src="logo.png" alt="logo" height="100px"> </a>
+    </li>
+
+    <li class: page> <a href="login.php" class="w3-bar-item w3-button">Login Here</a> </li>
+            <li class: page > <a target = "_blank" href="register.php" class="w3-bar-item w3-button">Register Here!</a> </li>
+           <li class: page>  <a target = "_blank" href="game1.htm" class="w3-bar-item w3-button">Go Home Gazeebo</a> </li>
+            <li class: page> <a target = "_blank" href="game2.htm" class="w3-bar-item w3-button">Mission: Molecule</a> </li>
+             <li class: page> <a target = "_blank" href="startpage.php" class="w3-bar-item w3-button">Go Back to the Homepage</a> </li>
+    <ul>
+       
     <?php
-    include "navbar.php";
+    // include "navbar.php";
 
 ?>
-     <!-- <nav>
-        <ul>
-                    <li>
-                        <a href="login.htm">Login Here!</a>
-                        <br>
-                        <a href="register.htm">Register Here!</a>
-                        <br>
-                        <a href="game1.htm">Go Home Gazeebo!</a>
-                        <br>
-                        <a href="game2.htm">Molecule's Mission!</a>
-                    </li>
-        </ul>
-        </nav> -->
+
 </body>
 </html>
