8a1950bc31fc2a75ff1eed69278b21abce4bb128
Jayla <delgado@etown.edu>
2025-08-05 10:07:14 -0400

    Updated CSS

--- Front-End/startpage.php
+++ Front-End/startpage.php
@@ -3,30 +3,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Start page</title>
-    <!-- <link href="navbar.htm"> -->
     <link href="navbar.css" rel="stylesheet">
 </head>
 
 <body>
-      <h1>Home Page</h1>
-     <li class: logo> 
-        <ul>
-                <!-- this is just a filler link until we get a better web space. -->
-        <a href="http://127.0.0.1/MCA25PROJECT1/Front-End/login.php" style="margin-top:0px">
-             <img id="logo" src="logo.png" alt="logo" height="100px"> </a>
-    </li>
-
-    <li class: page> <a href="login.php" class="w3-bar-item w3-button">Login Here</a> </li>
-            <li class: page > <a target = "_blank" href="register.php" class="w3-bar-item w3-button">Register Here!</a> </li>
-           <li class: page>  <a target = "_blank" href="game1.htm" class="w3-bar-item w3-button">Go Home Gazeebo</a> </li>
-            <li class: page> <a target = "_blank" href="game2.htm" class="w3-bar-item w3-button">Mission: Molecule</a> </li>
-             <li class: page> <a target = "_blank" href="startpage.php" class="w3-bar-item w3-button">Go Back to the Homepage</a> </li>
-    <ul>
-       
+    
+    <nav class="main-nav"> 
+        <ul class="nav-list">
+           
+            <li class="nav-item page"> <a href="login.php" class="w3-bar-item w3-button">Login Here</a> </li>
+            <li class="nav-item page"> <a target="_blank" href="register.php" class="w3-bar-item w3-button">Register Here!</a> </li>
+            <li class="nav-item logo"> 
+                <a href="http://127.0.0.1/MCA25PROJECT1/Front-End/login.php">
+                    <img id="logo" src="logo.png" alt="logo" height="200px" style="position: relative; top: 10px; left: 10%">
+                </a>
+            </li>
+            <li class="nav-item page"> <a target="_blank" href="game1.htm" class="w3-bar-item w3-button">Go Home Gazeebo</a> </li>
+            <li class="nav-item page"> <a target="_blank" href="game2.htm" class="w3-bar-item w3-button">Mission: Molecule</a> </li>
+            <!-- <li class="nav-item page"> <a target="_blank" href="startpage.php" class="w3-bar-item w3-button">Go Back to the Homepage</a> </li> -->
+        </ul>
+    </nav>
+    <h1>Home Page</h1>
     <?php
     // include "navbar.php";
-
-?>
-
+    ?>
 </body>
 </html>
