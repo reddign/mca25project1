85ed92761985c5968252ad2af1f0a36564dc8438
SF-rj <Lefever@etown.edu>
2025-08-06 08:30:18 -0700

    Adding background, and bird to game

--- Molecule's_Game/game2.js
+++ Molecule's_Game/game2.js
@@ -1,81 +1,117 @@
+/*************************************************************************************************************************
+    Programmer: Ryan L
+    Git: SF-rj
+************************************************************************************************************************/
 //Gets the HTML object canvas from webpage (document)
-let canvas = document.querySelector("canvas");
+    let canvas = document.querySelector("canvas");
 //Gets the graphics
-const graphics = canvas.getContext("2d");
+    const graphics = canvas.getContext("2d");
 //Declare Variables
-let rockX= 800;
-let rockY = 479;
-let y = 408;
-let x = 100;
-let fps = 3;
-let frame = 1
+    let rockX= 1010;
+    let rockY = 405;
+    let birdX = 500;
+    let birdY = 260;
+    let y = 370;
+    let x = 100;
+    let fps = 8;
+    let mFrames = 1
+    let bFrames = 1;
+    let speed = 50;
 //Molecule sprites
-const mImage1 = new Image();
-mImage1.src = "Molecule_1.png"
-const mImage2 = new Image();
-mImage2.src = "Molecule_2.png"
-const mImage3 = new Image();
-mImage3.src = "Molecule_3.png"
+    const mImage1 = new Image();
+    mImage1.src = "Molecule_1.png"
+    const mImage2 = new Image();
+    mImage2.src = "Molecule_2.png"
+    const mImage3 = new Image();
+    mImage3.src = "Molecule_3.png"
+    const rImage = new Image();
+    rImage.src = "Rock_optimized.png"
+//Bird sprites
+    const bImage1 = new Image();
+    bImage1.src = "Bird_1.png"
+    const bImage2 = new Image();
+    bImage2.src = "Bird_2.png"
+//Backgrounds
+    const backgrounds = [];
+    const back1 = new Image();
+    back1.src = "Background_1.png";
+    backgrounds.push(back1);
+    const back2 = new Image();
+    back2.src = "Background_2.png";
+    backgrounds.push(back2);
+    const back3 = new Image();
+    back3.src = "Background_3.png";
+    backgrounds.push(back3);
 
-
-
-function animation()
-{
-clear();
-rock();
-rockX -= 50;
-moleculeImage(x,y);
-jump();
-if(y < 408)
-{
-y+= 20;
-}
-
-}
-
-
-function rock()
-{
-    graphics.fillStyle = "gray";
-    graphics.beginPath();
-    graphics.arc(rockX,rockY,20,0,Math.PI*2);
-    graphics.fill();
-    graphics.closePath();
-
-}
-function jump(event)
-{
-    document.addEventListener('keyup', function(event) 
+//Animation function to make the game move
+    function animation()
     {
- 
-    if (y == 408){
-        if(event.key === 'Enter')
+        clear();
+        rock();
+        rockX -= speed;
+        moleculeImage(x,y);
+        birdImage(birdX,birdY);
+        jump();
+        if(y < 370)
+        {y+= 20;}
+        if(rockX < 0)
+        {rockX = 1010;}
+    }
+//Function to draw a rock
+    function rock()
+    {
+        graphics.drawImage(rImage,rockX,rockY,60,60);
+    }
+//Function to make character jump
+    function jump(event)
+    {
+        document.addEventListener('keyup', function(event) 
         {
-             y = 328;
+    
+        if (y == 370){
+            if(event.key === 'Enter')
+            {
+                y = 290;
+            }
         }
     }
-}
-    )
-}
+        )
+    }
+//Molecule images
+    function moleculeImage(x,y)
+    {
+        if(mFrames == 1 || mFrames == 3)
+        {graphics.drawImage(mImage1,x,y,100,100);}
+        else if(mFrames == 2)
+        {graphics.drawImage(mImage2,x,y,100,100);}
+        else
+        {graphics.drawImage(mImage3,x,y - 8,100,100);}
+        mFrames++;
+        if(mFrames > 4 ){
+            mFrames = 1 ;
+        }
+    }
+     function birdImage(x,y)
+    {
+        if(bFrames == 0)
+        {graphics.drawImage(bImage1,x,y,60,60);}
+        else(bFrames == 2)
+        {graphics.drawImage(bImage2,x,y,60,60);}
+        bFrames++;
+        if(bFrames > 2){
+            bFrames = 0 ;
+        }
+    }
+//Bird images
+    function bird(){
 
-function moleculeImage(x,y)
-{
-if(frame == 1 || frame == 3)
-{graphics.drawImage(mImage1,x,y,100,100);}
-else if(frame == 2)
-{graphics.drawImage(mImage2,x,y,100,100);}
-else
-{graphics.drawImage(mImage3,x,y - 8,100,100);}
-frame++;
-if(frame > 4 ){
-    frame = 1 ;
-}
-}
+    }
 //Clear
 function clear()
 {
-    graphics.fillStyle = "#FFEEDD";
-    graphics.fillRect(0,0,canvas.width,canvas.height);
+   
+    graphics.drawImage(backgrounds[0],0,0,1000,500);
+
 }
 
 
