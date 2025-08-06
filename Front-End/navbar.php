3ee6f1808a53e77f8c01a43122386fdc659c2287
Charlotte Cohick <cohick@etown.edu>
2025-08-06 09:54:24 -0400

    index erm

--- Front-End/navbar.php
+++ Front-End/navbar.php
@@ -1,12 +1,18 @@
-<div class="w3-top">
-        <div class="w3-bar w3-blue w3-padding w3-card" style="letter-spacing:4px;">
-          <a href="../index.php" class="w3-bar-item w3-button">Go Back to the Homepage</a>
+<link href="navbar.css" rel="stylesheet">
+
+
+<nav class="main-nav">
+<div>
+        <div class="nav-list" style="letter-spacing:4px;">
+          <li><a href="index.php" class="w3-bar-item w3-button"><img src="Front-End/logo.png" class="nav-item.logo"></a></li>
           <!-- Right-sided navbar links. Hide them on small screens -->
-          <div class="w3-right w3-hide-small">
-            <a href="login.php" class="w3-bar-item w3-button">Login Here</a>
+          <div class="nav-list">
+            <li><a href="login.php" class="w3-bar-item w3-button">Login Here</a></li>
             <a href="register.php" class="w3-bar-item w3-button">Register Here!</a>
+          </div><div class="nav-list">
             <a href="game1.htm" class="w3-bar-item w3-button">Go Home Gazeebo</a>
              <a href="game2.htm" class="w3-bar-item w3-button">Mission: Molecule</a>
           </div>
         </div>
       </div>
+</nav>
